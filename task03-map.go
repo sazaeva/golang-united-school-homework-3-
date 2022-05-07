9a6b0ed1dda2671b1419d97ece40f4239cfa43ef
sazaeva <janylsazaeva@gmail.com>
2022-05-07 23:02:05 +0300

    Fixes

--- task03-map.go
+++ task03-map.go
@@ -5,7 +5,7 @@ import (
 	"sort"
 )
 
-func SortMapValues(input map[int]string) (result []string) {
+func sortMapValues(input map[int]string) (result []string) {
 	keys := make([]int, 0, len(input))
 	for k := range input {
 		keys = append(keys, k)
