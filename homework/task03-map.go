d2f28b816773b61459d5fa2aefef57520a779879
sazaeva <janylsazaeva@gmail.com>
2022-05-07 22:46:56 +0300

    Fixed path

--- homework/task03-map.go
+++ homework/task03-map.go
@@ -5,14 +5,14 @@ import (
 	"sort"
 )
 
-func sortMapValues(input map[int]string) (result []string) {
+func SortMapValues(input map[int]string) (result []string) {
 	keys := make([]int, 0, len(input))
 	for k := range input {
 		keys = append(keys, k)
 	}
 	sort.Ints(keys)
 	for _, k := range keys {
-		fmt.Println(k, input[k])
+		fmt.Printf("%s ", input[k])
 
 	}
 	return
